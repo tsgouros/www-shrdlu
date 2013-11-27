a5bd1b240be32d11799f046b77c11d0b428a3b93
tsgouros <tsgouros@gmail.com>
2013-11-26 19:26:55 -0500

    interim

--- render/oview.js
+++ render/oview.js
@@ -5,24 +5,154 @@
 //   move hand
 
 var container, stats;
-var camera, scene, renderer;
+var camera, scene, renderer, projector;
+var objs = new blockCollection();
 
 init();
 animate();
 
-function block(type, color, dimX, dimY, dimZ, posX, posY, posZ) {
-    this.type = type;
+function blockCollection() {
+    this.set = {};
+    this.current = -1;
+    this.nobjs = 0;
+
+    this.add = add;
+    this.getCurrent = getCurrent;
+    this.getKeys = getKeys;
+    this.getNth = getNth;
+
+
+    function add(name, color, dimension, position) {
+	this.set[name] = new block(name, color, dimension, position);
+	this.nobjs += 1;
+    }
+
+    // returns an object and advances to the next one.
+    function getCurrent() {
+	if (this.set.length < 1) 
+	{ 
+	    console.log("uh-oh");
+	    return null; 
+	} 
+	else 
+	{
+	    this.current += 1;
+
+	    if (this.current >= Object.keys(this.set).length)
+	    {
+		this.current = 0;
+	    }
+
+	    return this.set[Object.keys(this.set)[this.current]];
+	}
+    }
+
+    function getKeys() {
+	return Object.keys(this.set);
+    }
+
+    function getNth(nth) {
+	return this.set[this.getKeys()[nth]];
+    }
+
+    // returns zero if no object is at position, otherwise the height
+    // of whatever is there.
+    function floor(position) {
+
+	var out = 0;
+	for (i = 0 ; i < nobjs; i++) 
+	{
+	    out = Math.max(out, this.getNth(i).onTop(position));
+	}
+    }
+}
+
+
+function block(name, color, dimension, position) {
+    this.name = name;
     this.color = color;
-    this.dimX = dimX;
-    this.dimY = dimY;
-    this.dimZ = dimZ;
-    this.posX = posX;
-    this.posY = posY;
-    this.posZ = posZ; }
+    this.dimension = dimension;
+    this.position = position;
+
+    this.render = render;
+    this.moveto = moveto;
+    this.pickup = pickup;
+    this.putdown = putdown;
+
+    this.onTop = onTop;
+
+    this.upflag = false;
+
+    var geometry = new THREE.CubeGeometry( dimension.x, 
+					   dimension.y, 
+					   dimension.z );
+
+    var material = new THREE.MeshLambertMaterial( 
+	{ color: color, 
+	  shading: THREE.FlatShading, 
+	  overdraw: .5 } );
+
+    this.obj = new THREE.Mesh( geometry, material );
+
+    this.obj.scale.y = 1;
+
+    this.obj.position.x = position.x;
+    this.obj.position.y = position.y;
+    this.obj.position.z = position.z;
+
+    function render() {}
+
+    // returns the height of the block if the x and z are on top of
+    // it, zero otherwise.
+    function onTop(position) {
+	if ((this.obj.position.x - (this.obj.dimension.x / 2) < position.x) &&
+	    (position.x < (this.obj.position.x + (this.obj.dimension.x / 2))))
+	{
+	    if ((this.obj.position.z - (this.obj.dimension.z / 2) < position.z) &&
+		(position.z < (this.obj.position.z + (this.obj.dimension.z / 2))))
+	    {
+		return this.obj.position.y + (this.obj.dimension.y / 2);
+	    }
+	}
+	return 0;
+    }
+
+    function moveto( target ) {
+
+	var tween = new TWEEN.Tween(this.obj.position).to(target, 2000);
+
+	tween.start();
+    }
+
+    function pickup() {
+
+	var target = { x: this.obj.position.x, 
+		       y: this.obj.position.y + 125, 
+		       z: this.obj.position.z };
+
+	this.upflag = true;
+
+	this.moveto(target);
+	
+    }
+
+    function putdown() {
+	var target = { x: this.obj.position.x, 
+		       y: this.obj.position.y - 125, 
+		       z: this.obj.position.z };
+
+	this.upflag = false;
+
+	this.moveto(target);
+    }
+
+}
 
 
 function init() {
-    
+
+    console.log("hello there");
+
     container = document.createElement( 'div' );
     document.body.appendChild( container );
 
@@ -34,7 +164,11 @@ function init() {
     info.innerHTML = '';
     container.appendChild( info );
 
-    camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000 );
+    camera = new THREE.OrthographicCamera( window.innerWidth / -2, 
+					   window.innerWidth / 2, 
+					   window.innerHeight / 2, 
+					   window.innerHeight / -2, 
+					   -500, 1000 );
     camera.position.x = 200;
     camera.position.y = 100;
     camera.position.z = 200;
@@ -65,32 +199,30 @@ function init() {
 
     // Cubes
 
-    var geometry = new THREE.CubeGeometry( 50, 50, 50 );
+    objs.add('cube1', Math.random() * 0xffffff, 
+		  {x: 50, y:50, z:50}, {x:0, y:25, z:0});
 
-    for ( var i = 0; i < 100; i ++ ) {
+    objs.add('cube2', Math.random() * 0xffffff, 
+		  {x: 50, y:50, z:50}, {x:100, y:25, z:100});
 
-	var material = new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff, shading: THREE.FlatShading, overdraw: 0.5 } );
+    objs.add('cube3', Math.random() * 0xffffff, 
+		  {x: 75, y:75, z:75}, {x:-100, y:37, z:100});
 
-	var cube = new THREE.Mesh( geometry, material );
+    for (i = 0; i < objs.nobjs; i++) {
 
-	cube.scale.y = Math.floor( Math.random() * 2 + 1 );
+	console.log(objs.getNth(i).name);
 
-	cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-	cube.position.y = ( cube.scale.y * 50 ) / 2;
-	cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-
-	scene.add( cube );
-	
+	scene.add(objs.getNth(i).obj);
     }
 
     // Lights
 
-    var ambientLight = new THREE.AmbientLight( 0x0a );
+    var ambientLight = new THREE.AmbientLight( 0x0c );
     scene.add( ambientLight );
 
     var directionalLight = new THREE.DirectionalLight( 0x888888 );
     directionalLight.position.x =  - 0.5;
-    directionalLight.position.y =  - 0.5;
+    directionalLight.position.y =  0.5;
     directionalLight.position.z =  - 0.5;
     directionalLight.position.normalize();
     scene.add( directionalLight );
@@ -102,6 +234,7 @@ function init() {
     directionalLight.position.normalize();
     scene.add( directionalLight );
 
+    projector = new THREE.Projector();
     renderer = new THREE.CanvasRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     
@@ -109,12 +242,28 @@ function init() {
     
     stats = new Stats();
     stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '50px';
+    stats.domElement.style.top = '0px';
     container.appendChild( stats.domElement );
 
     //
 
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
     window.addEventListener( 'resize', onWindowResize, false );
+
+function onDocumentMouseDown( event ) {
+
+    event.preventDefault();
+
+    var obj = objs.getCurrent();
+
+    if (obj.upflag) {
+	obj.putdown(); 
+    } else {
+	obj.pickup();
+    }
+
+}
+
 
 }
 
@@ -133,6 +282,48 @@ function onWindowResize() {
 
 //
 
+
+
+
+// function onDocumentMouseDown( event ) {
+
+//     event.preventDefault();
+
+//     var vector = new THREE.Vector3( 
+// 	( event.clientX / window.innerWidth ) * 2 - 1, 
+// 	    - ( event.clientY / window.innerHeight ) * 2 + 1, 
+// 	0.5 );
+//     projector.unprojectVector( vector, camera );
+
+//     var raycaster = new THREE.Raycaster( 
+// 	camera.position, 
+// 	vector.sub( camera.position ).normalize() );
+
+//     var intersects = raycaster.intersectObjects( scene.children );
+
+//     console.log(intersects[0]);
+
+//     if ( intersects.length > 0 ) {
+
+// 	new TWEEN.Tween( intersects[ 0 ].object.position )
+// 	    .to( {
+// 		x: intersects[0].object.position.x,
+// 		y: Math.random() * 125,
+// 		z: intersects[0].object.position.z }, 2000 )
+// 	    .easing( TWEEN.Easing.Elastic.Out).start();
+
+/*	new TWEEN.Tween( intersects[ 0 ].object.rotation ).to( {
+	    x: Math.random() * 2 * Math.PI,
+	    y: Math.random() * 2 * Math.PI,
+	    z: Math.random() * 2 * Math.PI }, 2000 )
+	    .easing( TWEEN.Easing.Elastic.Out).start(); */
+
+//     }
+
+// }
+
+
+
 function animate() {
 
     requestAnimationFrame( animate );
@@ -140,13 +331,14 @@ function animate() {
     render();
     stats.update();
 
+    TWEEN.update();
 }
 
 function render() {
 
     var timer = Date.now() * 0.0001;
 
-    camera.position.x = Math.cos( timer ) * 50;
+    camera.position.x = Math.cos( timer ) * 200;
     camera.position.z = Math.sin( timer ) * 200;
     camera.lookAt( scene.position );
     
