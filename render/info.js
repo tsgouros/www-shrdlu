a6d287f3133a51a24dfaa410165083b1d9552cf5
tsgouros <tsgouros@gmail.com>
2014-01-31 12:40:39 -0500

    adjusted for load-balancing use

--- render/info.js
+++ render/info.js
@@ -37,7 +37,7 @@ $(function()
 
 //	    var ralph = "hello"; //this.serialize();
 
-	    var pushURL = "http://block.cs.brown.edu:1229?cmd=" + 
+	    var pushURL = "http://block.cs.brown.edu?cmd=" + 
 		encodeURIComponent(tmp);
 
 	    var xmlHttp = new XMLHttpRequest();
