0c60f79b7f401f6326cbd3b6641170ec9ecee844
tsgouros <tsgouros@gmail.com>
2014-02-09 23:18:37 -0500

    added load-path as third arg

--- src/shrdlu.lisp
+++ src/shrdlu.lisp
@@ -1,18 +1,33 @@
-#!/usr/bin/clisp
+#!/opt/local/bin/clisp
 ;
 ; This file loads in all the needed files for SHRDLU.
 ;
 (defpackage shrdlu
   (:use :common-lisp))
 (in-package :shrdlu)
-
+(princ "starting")
 ; This appears to be the width of the terminal
 ; No, it's not.  Not sure what it is....
 (DEFCONSTANT CHRCT 76)
 
-(setq custom:*load-paths* (cons "/home/tsgouros/shrdlu/src/" custom:*load-paths*))
-
+;; Third argument is the load path, i.e. the directory in which the
+;; source code lives.
+(if (car EXT:*ARGS*)
+    (setq custom:*load-paths* (cons (car EXT:*ARGS*) custom:*load-paths*)))
 (princ custom:*load-paths*)
+
+;; Parse and distribute the next two input arguments, if present.
+;; These identify the server and port to which the move commands are
+;; to be delivered.
+(if (caddr EXT:*ARGS*) 
+    (progn
+      (setq remote-cmds-p t)
+      (setq remote-actions-p t)
+      (setq remote-host (cadr EXT:*ARGS*))
+      (setq remote-port (parse-integer (caddr EXT:*ARGS*))) )
+    (progn
+      (setq remote-cmds-p nil)
+      (setq remote-actions-p nil)))
 
 (load "remote")
 (load "fixes")
@@ -37,18 +52,6 @@
 ; Set either (DEBUGMODE) or (USERMODE) here.
 (USERMODE)
 ;(DEBUGMODE)
-
-;; Parse and distribute the input arguments.  These identify the
-;; server and port to which the move commands are to be delivered.
-(if (cadr EXT:*ARGS*) 
-    (progn
-      (setq remote-cmds-p t)
-      (setq remote-actions-p t)
-      (setq remote-host (car EXT:*ARGS*))
-      (setq remote-port (parse-integer (cadr EXT:*ARGS*))) )
-    (progn
-      (setq remote-cmds-p nil)
-      (setq remote-actions-p nil)))
 
 ; Start the program
 (INITIALSTUFF 'UMR-1.0 NIL)
